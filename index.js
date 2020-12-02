43dbb27e0dbe7b0b3895f17d93e265973a81bf1b
cyamos2 <cyamos2@yahoo.com>
2020-12-01 19:23:52 -0700

    Worked on tasks 1 to 5

--- index.js
+++ index.js
@@ -28,6 +28,7 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
+  Counter 1 covers everything while counter 2 is established in a global area.
   
   2. Which of the two uses a closure? How can you tell?
   
@@ -62,9 +63,10 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+return Math.floor(Math.random() * Math.floor(2));
 }
+console.log('Task 2', inning());
 
 
 /* ⚾️⚾️⚾️ Task 3: finalScore() ⚾️⚾️⚾️
@@ -81,17 +83,42 @@ Use the finalScore function below to do the following:
 }
 */ 
 
-function finalScore(/*code Here*/){
-  /*Code Here*/
+function finalScore(inCB, inPlays){
+  let score = {
+    Home: 0,
+    Away: 0,
+  }
+  for (let i=0; i<inPlays; i++) {
+  score.Home = score.Home + inCB();
+  score.Away = score.Away + inCB();
+  }
+  return score;
 }
+console.log('task 3',finalScore(inning,9));
 
 /* ⚾️⚾️⚾️ Task 4: getInningScore() ⚾️⚾️⚾️
 Use the getInningScore() function below to do the following:
   1. Receive a callback function - you will pass in the inning function from task 2 as your argument 
   2. Return an object with a score for home and a score for away that populates from invoking the inning callback function */
 
-function getInningScore(/*Your Code Here */) {
-  /*Your Code Here */
+function getInningScore(awayScore, homeScore) {
+  return awayScore + " - " + homeScore;
+}
+
+function scoreboard(getInningScore, inning, number0fInnings){
+  var awayScore;
+  var homeScore;
+  var currentInning = 0;
+
+  for (let i = 0; i < number0fInnings; i++) {
+    awayScore = inning();
+    homeScore = inning();
+    if (currentInning === 1) {
+      currentInning + "st inning:" + awayScore + " - " + homeScore;
+    }
+    else if (currentInning === 2){
+    } return `${currentInning} ${homeScore},n"Away": ${awayScore},`;
+  }
 }
 
 
@@ -136,9 +163,18 @@ Use the scoreboard function below to do the following:
 ]  
   */
 
-function scoreboard(/* CODE HERE */) {
-  /* CODE HERE */
+function scoreboard(inning, innings) {
+  let homeScore = 0;
+  let awayScore = 0;
+
+  for (let i=1 ; i <= innings ; i++) {
+    homeScore += inning();
+    awayScore += inning();
+  }
 }
+
+console.log('Inning ${i} | Home: ${homeScore} | Away: ${awayScore}');
+console.log(`Final Score | Home: ${homeScore} | Away: ${awayScore}');
 
 
 
